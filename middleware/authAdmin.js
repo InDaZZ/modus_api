4beaf203c17d56b51f4e790e7ac115ce6f5a8531
Evgeny Dekhtyarev <ol9696@bk.ru>
2024-01-09 18:16:27 +0300

    багфикс предидущего коммита

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -1,17 +1,17 @@
-const jwt = require('jsonwebtoken');
 const admiUser = require('../model/adminUser');
 
 module.exports = (req, res, next) => {
     console.log(11111)
+    console.log(admiUser)
     const authorization = req.cookies.id;//После написания фронта буду брать токен из localstorage
 
     if (!authorization) {
         return res.send('Данные пользователя неверны');
     }
-    admiUser.findById(findById)
+    admiUser.findById(authorization)
         .then((user) => {
             if (user.admin === true) {
-                req.adminId = findById
+                req.adminId = authorization
                 return next();
             }
             throw new Error('Данные пользователя неверны');
